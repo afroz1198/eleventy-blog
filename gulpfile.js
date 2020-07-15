273b10f72a3b04a185405eaca556597002f4ed72
afroz1198 <mohammedafroz.1198@gmail.com>
2020-07-15 21:30:46 +0530

    gulp is awesome

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+// a task to generate the css with sass
+gulp.task('css', function() {
+	return gulp
+		.src('scss/index.scss')
+		.pipe(
+			sass({
+				outputStyle: 'compressed'
+			}).on('error', sass.logError)
+		)
+		.pipe(gulp.dest('css/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('scss/**/*.scss', gulp.parallel('css'));
+});
