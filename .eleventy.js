bbc94d22a841c78f669c4b0348169a8e7e451da2
afroz1198 <mohammedafroz.1198@gmail.com>
2020-08-23 14:08:33 +0530

    getting structured

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,8 @@ const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginNavigation = require('@11ty/eleventy-navigation');
 const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
-let pluginEmbedTweet = require('eleventy-plugin-embed-tweet');
-
-let tweetEmbedOptions = {
-	cacheDirectory: '', // default: ''
-	useInlineStyles: true, // default: true
-	autoEmbed: false // default: false
-};
 
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPlugin(pluginEmbedTweet, tweetEmbedOptions);
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(pluginSyntaxHighlight);
 	eleventyConfig.addPlugin(pluginNavigation);
@@ -41,7 +33,7 @@ module.exports = function(eleventyConfig) {
 		return array.slice(0, n);
 	});
 
-	eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
+	eleventyConfig.addCollection('tagList', require('./tags/getTagList'));
 
 	eleventyConfig.addPassthroughCopy('img');
 	eleventyConfig.addPassthroughCopy('css');
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+gulp.task('minify_prism_theme', function() {
+	console.log(sass.compiler);
+	return gulp
+		.src('css/prism-base16-monokai.dark.css')
+		.pipe(
+			sass({
+				outputStyle: 'compressed'
+			}).on('error', sass.logError)
+		)
+		.pipe(gulp.dest('css/'));
+});
+
 // a task to generate the css with sass
-gulp.task('css', function() {
+gulp.task('sass', function() {
 	return gulp
 		.src('scss/index.scss')
 		.pipe(
@@ -12,7 +24,6 @@ gulp.task('css', function() {
 		)
 		.pipe(gulp.dest('css/'));
 });
-
 gulp.task('watch', function() {
-	gulp.watch('scss/**/*.scss', gulp.parallel('css'));
+	gulp.watch('scss/**/*.scss', gulp.parallel([ 'sass' ]));
 });
